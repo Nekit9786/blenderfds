c7331132cf398783005d934d06fa7c982fa1df3a
Emanuele Gissi <emanuele.gissi@gmail.com>
2016-12-26 10:51:02 +0100

    Bump version

--- zzz_blenderfds/__init__.py
+++ zzz_blenderfds/__init__.py
@@ -25,7 +25,7 @@ print("""
 bl_info = {
     "name": "BlenderFDS",
     "author": "Emanuele Gissi",
-    "version": (4,1,1),
+    "version": (4,1,2),
     "blender": (2,7,6),
     "api": 35622,
     "location": "File > Export > FDS Case (.fds)",
