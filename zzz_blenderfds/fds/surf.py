5c8c673a96cfe4e99890744b7bf2294c7476a45d
emanuele.gissi@gmail.com <emanuele.gissi@gmail.com>
2018-05-10 11:40:47 -0400

    Add PERIODIC predefined SURF

--- zzz_blenderfds/fds/surf.py
+++ zzz_blenderfds/fds/surf.py
@@ -2,7 +2,7 @@
 
 import bpy
 
-predefined = ("INERT", "OPEN", "MIRROR", "HVAC")
+predefined = ("INERT", "OPEN", "MIRROR", "HVAC", "PERIODIC")
 
 def has_predefined():
     """Check predefined materials"""
@@ -12,8 +12,14 @@ def set_predefined(context):
     """Set BlenderFDS predefined materials/bcs"""
     mas = bpy.data.materials.keys()
     value = str()
-    if "INERT"  not in mas: value += "&SURF ID='INERT'  RGB=204,204,51 FYI='Predefined SURF' /\n"
-    if "OPEN"   not in mas: value += "&SURF ID='OPEN'   RGB=51,204,204 FYI='Predefined SURF' TRANSPARENCY=.2 /\n"
-    if "MIRROR" not in mas: value += "&SURF ID='MIRROR' RGB=51,51,204  FYI='Predefined SURF' /\n"
-    if "HVAC"   not in mas: value += "&SURF ID='HVAC'   RGB=51,51,204  FYI='Predefined SURF' /\n"
+    if "INERT" not in mas:
+        value += "&SURF ID='INERT' RGB=204,204,51 /\n"
+    if "OPEN" not in mas:
+        value += "&SURF ID='OPEN' RGB=51,204,204 TRANSPARENCY=.2 /\n"
+    if "PERIODIC" not in mas:
+        value += "&SURF ID='PERIODIC' RGB=204,204,204 TRANSPARENCY=.2 /\n"
+    if "MIRROR" not in mas:
+        value += "&SURF ID='MIRROR' RGB=51,51,204 /\n"
+    if "HVAC" not in mas:
+        value += "&SURF ID='HVAC' RGB=51,51,204 /\n"
     if value: context.scene.from_fds(context, value)
