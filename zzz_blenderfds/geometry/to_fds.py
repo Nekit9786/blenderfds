e915216ad5ba114565b3ba24a81e10d821e2d833
Emanuele Gissi <emanuele.gissi@gmail.com>
2018-08-08 16:52:28 +0200

    Clean-up and removal of GEOM geometric code

--- zzz_blenderfds/geometry/to_fds.py
+++ zzz_blenderfds/geometry/to_fds.py
@@ -1,26 +1,27 @@
 """BlenderFDS, translate Blender object geometry to FDS notation."""
 
-import bpy, bmesh
+import bpy
 from time import time
-from .geom_utils import *
-from .voxelize import voxelize, pixelize
+from . import utils
+from .calc_voxels import get_voxels, pixelize
+from .calc_trisurfaces import get_trisurface
 from ..exceptions import BFException
 
 DEBUG = True
 
-### to None
+#++ to None
 
 def ob_to_none(context, ob):
     DEBUG and print("BFDS: geometry.ob_to_none:", ob.name)
     return (), ""
 
-### to XB
+#++ to XB
 
 def ob_to_xbs_voxels(context, ob) -> "((x0,x1,y0,y1,z0,z1,), ...), 'Message'":
     """Transform ob solid geometry in XBs notation (voxelization). Never send None."""
     DEBUG and print("BFDS: geometry.ob_to_xbs_voxels:", ob.name)
     t0 = time()
-    xbs, voxel_size, timing = voxelize(context, ob)
+    xbs, voxel_size, timing = get_voxels(context, ob)
     if not xbs: return (), "No voxel created"
     msg = "{0} voxels, resolution {1:.3f} m, in {2:.3f} s".format(len(xbs), voxel_size, time()-t0)
     if DEBUG: msg += " (s:{0[0]:.3f} 1f:{0[1]:.3f}, 2g:{0[2]:.3f}, 3g:{0[3]:.3f})".format(timing)
@@ -42,7 +43,7 @@ def ob_to_xbs_bbox(context, ob) -> "((x0,x1,y0,y1,z0,z1,), ...), 'Message'":
     x0, x1, y0, y1, z0, z1 = get_global_bbox(context, ob)
     return [(x0, x1, y0, y1, z0, z1,),], ""
 
-def ob_to_xbs_faces(context, ob) -> "((x0,x1,y0,y1,z0,z0,), ...), 'Message'": # TODO use BMesh
+def ob_to_xbs_faces(context, ob) -> "((x0,x1,y0,y1,z0,z0,), ...), 'Message'":
     """Transform ob faces in XBs notation (faces). Never send None."""
     DEBUG and print("BFDS: geometry.ob_to_xbs_faces:", ob.name)
     # Init
@@ -61,9 +62,12 @@ def ob_to_xbs_faces(context, ob) -> "((x0,x1,y0,y1,z0,z0,), ...), 'Message'": # 
             bbmaxx, bbmaxy, bbmaxz = max(bbmaxx, x), max(bbmaxy, y), max(bbmaxz, z)
         bbd = [(bbmaxx - bbminx, 2), (bbmaxy - bbminy, 1), (bbmaxz - bbminz, 0)]
         bbd.sort()
-        if bbd[0][1] == 2: bbmaxx = bbminx = (bbminx+bbmaxx)/2
-        if bbd[0][1] == 1: bbmaxy = bbminy = (bbminy+bbmaxy)/2
-        if bbd[0][1] == 0: bbmaxz = bbminz = (bbminz+bbmaxz)/2
+        if bbd[0][1] == 2:
+            bbmaxx = bbminx = (bbminx+bbmaxx)/2
+        if bbd[0][1] == 1:
+            bbmaxy = bbminy = (bbminy+bbmaxy)/2
+        if bbd[0][1] == 0:
+            bbmaxz = bbminz = (bbminz+bbmaxz)/2
         result.append((bbminx, bbmaxx, bbminy, bbmaxy, bbminz, bbmaxz,),)
     result.sort()
     # Clean up
@@ -72,7 +76,7 @@ def ob_to_xbs_faces(context, ob) -> "((x0,x1,y0,y1,z0,z0,), ...), 'Message'": # 
     msg = len(result) > 1 and "{0} faces".format(len(result)) or ""
     return result, msg
 
-def ob_to_xbs_edges(context, ob) -> "((x0,x1,y0,y1,z0,z1,), ...), 'Message'": # TODO use BMesh
+def ob_to_xbs_edges(context, ob) -> "((x0,x1,y0,y1,z0,z1,), ...), 'Message'":
     """Transform ob faces in XBs notation (faces). Never send None."""
     DEBUG and print("BFDS: geometry.ob_to_xbs_edges:", ob.name)
     # Init
@@ -92,7 +96,7 @@ def ob_to_xbs_edges(context, ob) -> "((x0,x1,y0,y1,z0,z1,), ...), 'Message'": # 
 
 # Caller function (ob.bf_xb)
 
-choose_to_xbs = {
+choice_to_xbs = {
     "NONE"   : ob_to_none,
     "BBOX"   : ob_to_xbs_bbox,
     "VOXELS" : ob_to_xbs_voxels,
@@ -106,12 +110,12 @@ def ob_to_xbs(context, ob) -> "((x0,x1,y0,y1,z0,z0,), ...), 'Message'":
     # not ob.get("ob_to_xbs_cache") -> precalc not available or modified input conditions
     DEBUG and print("BFDS: geometry.ob_to_xbs:", ob.name)
     if not ob.get("ob_to_xbs_cache"): # ob.is_updated does not work here, checked in the handler
-        ob["ob_to_xbs_cache"] = choose_to_xbs[ob.bf_xb](context, ob) # Calculate
+        ob["ob_to_xbs_cache"] = choice_to_xbs[ob.bf_xb](context, ob) # Calculate
     return ob["ob_to_xbs_cache"]
 
-### to XYZ
+#++ to XYZ
 
-def ob_to_xyzs_vertices(context, ob) -> "((x0,y0,z0,), ...), 'Message'": # TODO use BMesh
+def ob_to_xyzs_vertices(context, ob) -> "((x0,y0,z0,), ...), 'Message'":
     """Transform ob vertices in XYZs notation. Never send None."""
     DEBUG and print("BFDS: geometry.ob_to_xyzs_vertices:", ob.name)
     # Init
@@ -135,7 +139,7 @@ def ob_to_xyzs_center(context, ob) -> "((x0,y0,z0,), ...), 'Message'":
 
 # Caller function (ob.bf_xyz)
 
-choose_to_xyzs = {
+choice_to_xyzs = {
     "NONE"     : ob_to_none,
     "CENTER"   : ob_to_xyzs_center,
     "VERTICES" : ob_to_xyzs_vertices,
@@ -146,11 +150,11 @@ def ob_to_xyzs(context, ob):
     # not ob.get("ob_to_xyzs_cache") -> precalc not available or modified input conditions
     DEBUG and print("BFDS: geometry.ob_to_xyzs:", ob.name)
     if not ob.get("ob_to_xyzs_cache"): # ob.is_updated does not work here, checked in the handler
-        ob["ob_to_xyzs_cache"] = choose_to_xyzs[ob.bf_xyz](context, ob) # Calculate
+        ob["ob_to_xyzs_cache"] = choice_to_xyzs[ob.bf_xyz](context, ob) # Calculate
     return ob["ob_to_xyzs_cache"]
 
 
-### to PB
+#++ to PB
 
 def ob_to_pbs_planes(context, ob) -> "(('X',x3,), ('X',x7,), ('Y',y9,), ...), 'Message'":
     """Transform ob faces in PBs notation. Never send None."""
@@ -158,12 +162,17 @@ def ob_to_pbs_planes(context, ob) -> "(('X',x3,), ('X',x7,), ('Y',y9,), ...), 'M
     # Init
     result = list()
     xbs, msg = ob_to_xbs_faces(context, ob)
+    epsilon = 1E-5
     # For each face build a plane...
     for xb in xbs:
-        if   abs(xb[1] - xb[0]) < epsilon: result.append((0,xb[0],),) # PBX is 0
-        elif abs(xb[3] - xb[2]) < epsilon: result.append((1,xb[2],),) # PBY is 1
-        elif abs(xb[5] - xb[4]) < epsilon: result.append((2,xb[4],),) # PBZ is 2
-        else: raise ValueError("BFDS: Building planes impossible, problem in ob_to_xbs_faces.")
+        if   abs(xb[1] - xb[0]) < epsilon:
+            result.append((0,xb[0],),) # PBX is 0
+        elif abs(xb[3] - xb[2]) < epsilon:
+            result.append((1,xb[2],),) # PBY is 1
+        elif abs(xb[5] - xb[4]) < epsilon:
+            result.append((2,xb[4],),) # PBZ is 2
+        else:
+            raise ValueError("BFDS: Building planes impossible, problem in ob_to_xbs_faces.")
     result.sort()
     # Nothing to clean up, return
     msg = len(result) > 1 and "{0} planes".format(len(result)) or ""
@@ -171,7 +180,7 @@ def ob_to_pbs_planes(context, ob) -> "(('X',x3,), ('X',x7,), ('Y',y9,), ...), 'M
 
 # Caller function (ob.bf_pb)
 
-choose_to_pbs = {
+choice_to_pbs = {
     "NONE"   : ob_to_none,
     "PLANES" : ob_to_pbs_planes,
 }
@@ -181,98 +190,13 @@ def ob_to_pbs(context, ob):
     # not ob.get("ob_to_pbs_cache") -> precalc not available or modified input conditions
     DEBUG and print("BFDS: geometry.ob_to_pbs:", ob.name)
     if not ob.get("ob_to_pbs_cache"): # ob.is_updated does not work here, checked in the handler
-        ob["ob_to_pbs_cache"] = choose_to_pbs[ob.bf_pb](context, ob) # Calculate
+        ob["ob_to_pbs_cache"] = choice_to_pbs[ob.bf_pb](context, ob) # Calculate
     return ob["ob_to_pbs_cache"]
 
-### to GEOM
+#++ to GEOM
 
-# Format:
-# &GEOM ID='FEM_MESH',
-#       SURF_ID='CONE','Gypsum plaster','Wood',
-#       MATL_ID='Matl1',
-#       VERTS=0.0699,-0.0146,3.4286,
-#             0.0714, 0.0000,3.4286,
-#             ...
-#       FACES=1,2,3, 1,
-#             4,5,2, 1,
-#             ...
-#       /
-
-# Get face 5 material:
-# ob.material_slots[bm.faces[5].material_index].material
-
-# Get linked Blender Material names list:
-# [material_slot.material.name for material_slot in ob.material_slots]
-
-# Get Material index in FDS format with bmesh for face 5:
-# bm.faces[5].material_index + 1
-
-def ob_to_geom(context, ob) -> "surf_idv, verts, faces":
-    """Transform Blender object geometry to new GEOM FDS notation. Never send a None."""
-
-    assert(ob.type == 'MESH')
-
-    # Get the new bmesh from the Object, apply modifiers, set in global coordinates, and triangulate
-    bm = bmesh.new()
-    bm.from_object(ob, context.scene, deform=True, render=False, cage=False, face_normals=True)
-    bm.transform(ob.matrix_world)
-    bmesh.ops.triangulate(bm, faces=bm.faces)
-
-    # Check for closed orientable non-intersecting manifolds, no degenerate geometry
-    epsilon = .000001 # FIXME global epsilon
-    # Check self intersection
-    import mathutils
-#    tree = mathutils.bvhtree.BVHTree.FromBMesh(bm, epsilon=epsilon)
-#    if tree.overlap(tree):
-#        raise BFException(ob, "Object self intersection detected.")
-    # Check edges:
-    # - manifold, each edge should join two faces, no more no less
-    # - contiguous normals, adjoining faces should have normals in the same directions
-    # - no degenerate edges, zero lenght edges
-    for edge in bm.edges:
-        if not edge.is_manifold:
-            raise BFException(ob, "Non manifold or open geometry detected.")
-        if not edge.is_contiguous:
-            raise BFException(ob, "Inconsistent face normals detected.")
-        if edge.calc_length() <= epsilon:
-            raise BFException(ob, "Too short edges detected.")
-    # Check degenerate faces, zero area faces
-#    for face in bm.faces:
-#        if face.calc_area() <= epsilon: raise BFException(ob, "Too small area faces detected.")
-    # Check loose vertices, vertices that have no connectivity
-    for vert in bm.verts:
-        if not bool(vert.link_edges): raise BFException(ob, "Loose vertices detected.")
-    # Check intersecting geometries # FIXME
-    # Check vertices: # FIXME
-    #  - manifold
-    #  - double Vertices
-    # Check non inverted normals # FIXME
-
-    # Get its surf_idv, and check that all used surfs are exported
-    surf_idv = []
-    for material_slot in ob.material_slots:
-        ma = material_slot.material
-        if not ma:
-            raise BFException(ob, "No referenced SURF, add Blender Material.")
-        if not ma.bf_export:
-            raise BFException(ob, "Referenced SURF ID='{}' is not exported.".format(ma.name))
-        surf_idv.append(ma.name)
-
-    # Get its vertex coordinates
-    verts = [(v.co.x, v.co.y, v.co.z) for v in bm.verts]
-
-    # Get its faces by vertex index
-    faces = [(
-        f.verts[0].index+1, # FDS index start from 1, not 0
-        f.verts[1].index+1,
-        f.verts[2].index+1,
-        f.material_index+1,
-        ) for f in bm.faces]
-
-    # Free the bmesh
-    bm.free()
-
-    # Set up msg
-    msg = "{} surf_id, {} vertices, {} faces".format(len(surf_idv),len(verts),len(faces))
-
-    return surf_idv, verts, faces, msg
+def ob_to_geom(context, ob) -> "mas, verts, faces, msg":
+    """Transform Blender object geometry to GEOM FDS notation. Never send a None."""
+    mas, verts, faces = get_trisurface(context, ob)
+    msg = "{} vertices, {} faces".format(len(verts), len(faces))
+    return mas, verts, faces, msg  # FIXME add caching of results
